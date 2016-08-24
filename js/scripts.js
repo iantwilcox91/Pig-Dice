b4a32e4f6390ca821e05673d14118a207523e582
Ian Wilcox and vichitra pool <ian+vichitra@>
2016-08-24 16:17:49 -0700

    roll functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
 // buisness logic
+var X = 0
 function diceRoll(){
   var rollResult = Math.floor((Math.random() * 6) + 1);
+  return rollResult ;
 }
-
-
-
-
+function dicePicture(index) {
+  var string = "dice/dice" + index + ".png";
+  $("img").attr("src", string);
+}
+function postCurrentRoll(index){
+  $("ul").append("<li>" + index + "</li>");
+}
+function totalCurrentPoints(i) {
+  X = X+i;
+  $(".CurPts").text(X);
+}
+function rolledOne(i) {
+  if (i === 1){
+    X = 0;
+    $(".CurPts").text(X);
+    $("ul").empty();
+    // switch player display
+  }
+}
 
 
 
@@ -17,7 +34,13 @@ $(document).ready(function() {
   $("form#player2name").submit(function(event) {
     event.preventDefault();
   });
-
+  $("#ROLLbtn").click(function() {
+    var numRolled = diceRoll();
+    dicePicture(numRolled);
+    postCurrentRoll(numRolled);
+    totalCurrentPoints(numRolled);
+    rolledOne(numRolled)
+  });
 
 
 
