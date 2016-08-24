6856ec4f5ce73745b1ef4b700a65875e544cd3b3
Ian Wilcox and vichitra pool <ian+vichitra@>
2016-08-24 14:37:17 -0700

    pig game

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// buisness logic
+function diceRoll(){
+  var rollResult = Math.floor((Math.random() * 6) + 1);
+}
+
+
+
+
+
+
+
+// front end logic
+$(document).ready(function() {
+  $("form#player1name").submit(function(event) {
+    event.preventDefault();
+  });
+  $("form#player2name").submit(function(event) {
+    event.preventDefault();
+  });
+
+
+
+
+
+
+
+
+});
